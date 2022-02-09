3d0c69eae67c3adec99498e0e0b5c1e14874b106
Kirubel berhanu <kirubelberhanu@gmail.com>
2022-02-09 12:13:40 +0300

    calculator add and diffrence function added

--- hello.cpp
+++ hello.cpp
@@ -1,1 +1,38 @@
+#include<iostream>
+#include<conio.h>
+#include<math.h>
+#include<stdlib.h>
+#include<iomanip>
+  char op;
+using namespace std;
+void sum()
+   {
+
+    int sum = 0;
+    int n;
+    int numberitems;
+    cout << "Enter number of items: \n";
+    cin >> numberitems;
+
+    for(int i=0;i<numberitems;i++)
+    {
+        cout<< "Enter number "<<i<<":\n\n" ;
+        cin>>n;
+        sum+=n;
+    }
+    cout<<"sum is: "<< sum<<endl<<endl;
+
+    }
+void diff()
+    {
+     int diff;
+     int n1,n2;
+     cout<<"enter two numbers to find their difference:\n\n";
+     cout<<"enter first number:";
+     cin>>n1;
+     cout<<"\nenter second number:";
+     cin>>n2;
+     diff=n1-n2;
+     cout<<"\ndifference is:"<<diff<<endl<<endl;
+     }
 
