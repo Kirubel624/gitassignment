8de10754d12c24b21e723861603ae7d9d235c612
leuul <75807425+leuul@users.noreply.github.com>
2022-02-10 15:33:29 +0300

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -59,3 +59,64 @@ void sq()
      s=sqrt(n);
      cout<<"\nsquare root of "<<n<<" is :"<<s<<endl;
      }
+int main()
+{
+
+
+    system("cls");
+    do
+    {
+
+    system("pause");
+    system("cls");
+    cout<<"***which operation you want to perform***\n";
+    cout<<"press 0 for exit\n";
+    cout<<"press 1 for addition \n";
+    cout<<"press 2 for subtraction\n";
+    cout<<"press 3 for multiplication\n";
+    cout<<"press 4 for division\n";
+    cout<<"press 5 for power calculation\n";
+    cout<<"press 6 for square root \n";
+    cout<<"press 7 for factorial calculation\n";
+    cout<<"press 8 for exponential calculation\n";
+    cout<<"press option:";
+    cin>>op;
+    switch(op)
+    {
+              case '1':
+              sum();
+
+              break;
+              case '2':
+              diff();
+              break;
+              case '3':
+              pro();
+              break;
+              case '4':
+              div();
+              break;
+              case '5':
+              power();
+              break;
+              case '6':
+              sq();
+              break;
+              case '7':
+              fact();
+              break;
+              case '8':
+              expo();
+              break;
+              case '0':
+              exit(0);
+              default:
+              cout<<"invalid input"  ;
+              system("cls");
+    }
+    }
+
+    while(op!='0');
+
+                    getch();
+                    }
